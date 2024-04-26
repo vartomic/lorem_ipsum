5383ee4414740c3dffa5112c7abc42b3d04828e5
vartomic <varrava16@mail.ru>
2024-04-26 12:26:42 +0300

    Added function wordCount2

--- lorem_ipsum/lorem_ipsum.cpp
+++ lorem_ipsum/lorem_ipsum.cpp
@@ -27,14 +27,46 @@ void wordCount(char* arr) {
         for (int j = 0; j < words.size(); j++) {
             if (words[j] == uniqueWords[i]) {
                 count++;
-            }           
+            }    
         }
-        std::cout << uniqueWords[i] << ' ' << count << std::endl;
+       std::cout << uniqueWords[i] << ' ' << count << std::endl;
+    }
+}
+
+void wordCount2(char* arr) {
+
+    std::vector<std::string> words;
+    char delimiters[] = " ,.;\n";
+    char* token = NULL;
+    char* ptr = NULL;
+
+    token = strtok_s(arr, delimiters, &ptr);
+
+    while (token != NULL) {
+        words.push_back(std::string(token));
+        token = strtok_s(NULL, delimiters, &ptr);
+    }
+
+    std::sort(words.begin(), words.end());
+
+    int count = 1;
+
+    for (int i = 0; i < words.size()-1; i++) {
+        auto j = i + 1;
+
+        if (words[i] == words[j]) {
+            count++;
+        }
+        else {
+            std::cout << words[i] << ' ' << count << std::endl;
+            count = 1;
+        }
     }
 }
 
 int main() {
-    wordCount(text);
+    //wordCount(text);
+    wordCount2(text);
 }
 
 
